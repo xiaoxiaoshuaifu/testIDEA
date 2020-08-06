2b344dc57fecee92d90b65f7c0371c29b5ce128b
zh_global <zh_git_global@qq.com>
2020-08-06 17:24:50 +0800

    提交

---
+++ src/main/java/com/nba/Test/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.nba.Test;
+
+/**
+ * @auther xiaoxiaoshuaifu
+ * @date 2020/8/6 15:27
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("测试类的信息");
+    }
+}
+
